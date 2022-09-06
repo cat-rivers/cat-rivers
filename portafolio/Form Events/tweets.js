805cb033d5cf2194d6e68960d6b02578a16f7808
Cat Rivers <53361608+cat-rivers@users.noreply.github.com>
2022-09-06 12:19:51 +0300

    Add files via upload

---
+++ portafolio/Form Events/tweets.js
@@ -0,0 +1,22 @@
+const tweetForm = document.querySelector("#tweetForm");
+const tweetList = document.querySelector("#tweetList");
+tweetForm.addEventListener("submit", function (e) {
+  //   const usernameInput = document.querySelectorAll("input")[0];
+  //   const tweetInput = document.querySelectorAll("input")[1];
+  e.preventDefault();
+
+  const usernameInput = tweetForm.elements.username;
+  const tweetInput = tweetForm.elements.tweet;
+  addTweet(usernameInput.value, tweetInput.value);
+  usernameInput.value = "";
+  tweetInput.value = "";
+});
+
+const addTweet = (username, tweet) => {
+  const newTweet = document.createElement("li");
+  const bTag = document.createElement("b");
+  bTag.append(username);
+  newTweet.append(bTag);
+  newTweet.append(`- ${tweet}`);
+  tweetList.append(newTweet);
+};
