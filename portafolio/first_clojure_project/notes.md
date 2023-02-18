bd8ea12e95bfe0abc63d67b79ac994414abe0891
Catalina Riveros <cat.rivers.blog@gmail.com>
2023-02-18 16:00:05 +0200

    trying clojure

---
+++ portafolio/first_clojure_project/notes.md
@@ -0,0 +1,5 @@
+### can I write that down on a notebook and youhelp me with the synmtax( toni says no) cati screams AAHHHHHH
+
+## something about native languages and bla bla looks, feels familiar, cannot read it
+
+##toni didnt know he could access the target value with the . . .
--- portafolio/first_clojure_project/src/main/first/app.cljs
+++ portafolio/first_clojure_project/src/main/first/app.cljs
@@ -1,44 +1,17 @@
 (ns first.app
-  (:require [helix.core :refer [defnc $]]
+  (:require [first.todo :as todo]
+            [helix.core :refer [defnc $]]
             [helix.hooks :as hooks]
             [helix.dom :as d]
             ["react-dom/client" :as rdom]))
 
-;; define components using the `defnc` macro
-(defnc greeting
-  "A component which greets a user."
-  [{:keys [name]}]
-  ;; use helix.dom to create DOM elements
-  (d/div "Hello, " (d/strong name) "!"))
-
-(defnc clickMe []
-  (d/button { :on-click #(js/alert "BOOO!!")} "Click Me!" )
-  )
-
-(doseq [val [ 2 3 4 5 6 7 8 9]
-        :when(odd? val)
-        ]
-       
-  
-  ( prn val)
-  )
-
 (defnc first-app []
-  (let [[state set-state] (hooks/use-state {:name "Helix User"})]
-    (d/div
-     (d/h1 "Welcome!")
-     ($ clickMe )
-      ;; create elements out of components
-      ($ greeting {:name (:name state)})
-      (d/input {:value (:name state)
-                :on-change #(set-state assoc :name (.. % -target -value))}))) 
-  )
+($ todo/todo-list-app))
 
+;; can be declare before defined
+(declare alert-cati)
 
+(defn alert-cati [] (js/alert "CATIII"))
 
-
-;; const clickMe () => {<button onClick = {js/alert ("Boo!!!")} > Click me </button>}
-
-;; start your app with your favorite React renderer
 (defonce root (rdom/createRoot (js/document.getElementById "app")))
 (.render root ($ first-app))
